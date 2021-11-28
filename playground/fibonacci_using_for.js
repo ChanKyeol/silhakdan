70bf07e41f3c0ca860d6ffb40504945c2d217c79
coldflow <dev.coldflow@gmail.com>
2021-11-29 00:27:43 +0900

    인덴트 재수정

--- playground/fibonacci_using_for.js
+++ playground/fibonacci_using_for.js
@@ -4,13 +4,13 @@ function fibo (idx) {
     if (idx == 1 || idx == 2) { //early return 패턴
       return 1;
     }
-    if( idx >= 3) {
+    if (idx >= 3) {
       let curNum = 1;
       let bfNum = 1;
-      for ( let i = 3; i <= idx; i++){
-      sumNum = bfNum + curNum;
-      bfNum = curNum;
-      curNum = sumNum;
+      for (let i = 3; i <= idx; i++) {
+        sumNum = bfNum + curNum;
+        bfNum = curNum;
+        curNum = sumNum;
       }
 
       return sumNum;
@@ -18,10 +18,5 @@ function fibo (idx) {
         
 }
 
-// console.log(fibo(3));
-// console.log(fibo(4));
-// console.log(fibo(5));
-// console.log(fibo(6));
-// console.log(fibo(20));
-// console.log(fibo(70));
+
 [3, 4, 5, 6, 20, 70].forEach((i) => console.log(i)); //forEach 사용으로 코드
