8b8309bcb25551e61bdf8ce148a0b9f5dca65a7b
coldflow <dev.coldflow@gmail.com>
2021-11-29 00:18:25 +0900

    foreach 사용

--- playground/fibonacci_using_for.js
+++ playground/fibonacci_using_for.js
@@ -5,8 +5,8 @@ function fibo (idx) {
       return 1;
     }
     if( idx >= 3) {
-      let curNum=1 ;
-      let bfNum=1 ;
+      let curNum=1;
+      let bfNum=1;
       for ( let i=1; i<=idx-2; i++){
       sumNum = bfNum + curNum;
       bfNum = curNum;
@@ -18,9 +18,10 @@ function fibo (idx) {
         
 }
 
-console.log(fibo(3));
-console.log(fibo(4));
-console.log(fibo(5));
-console.log(fibo(6));
-console.log(fibo(20));
-console.log(fibo(70));
+// console.log(fibo(3));
+// console.log(fibo(4));
+// console.log(fibo(5));
+// console.log(fibo(6));
+// console.log(fibo(20));
+// console.log(fibo(70));
+[3, 4, 5, 6, 20, 70].forEach((i) => console.log(i)); //forEach 사용으로 코드
