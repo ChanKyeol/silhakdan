50b22be7a5c4048875e775dbd823fb76238de1e1
coldflow <dev.coldflow@gmail.com>
2021-11-29 00:07:09 +0900

    인덴트 수정

--- playground/fibonacci_using_for.js
+++ playground/fibonacci_using_for.js
@@ -1,22 +1,22 @@
 function fibo (idx) {
-    let sumNum = 0;
+  let sumNum = 0;
     
-        if (idx == 1 || idx == 2) {
-            return 1
-        } else if( idx >= 3) {
-            let curNum=1 ;
-            let bfNum=1 ;
-            for ( let i=1; i<=idx-2; i++){
-    
-                sumNum = bfNum + curNum;
-                bfNum = curNum;
-                curNum = sumNum;
-    
-            }
-            return sumNum;
-        }
-        
+    if (idx == 1 || idx == 2) { //early return 패턴
+      return 1;
     }
+    if( idx >= 3) {
+      let curNum=1 ;
+      let bfNum=1 ;
+      for ( let i=1; i<=idx-2; i++){
+      sumNum = bfNum + curNum;
+      bfNum = curNum;
+      curNum = sumNum;
+      }
+
+      return sumNum;
+    }
+        
+}
 
 console.log(fibo(3));
 console.log(fibo(4));
