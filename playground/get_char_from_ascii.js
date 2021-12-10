dd79ed0cd9c52025d873530aeb5ba4370c42f235
coldMater <coldmater@gmail.com>
2021-12-11 02:09:42 +0900

    1차 완성

--- playground/get_char_from_ascii.js
+++ playground/get_char_from_ascii.js
@@ -17,19 +17,11 @@ function muchNum(num) {
 }
 
 function getFromAscii(ac){
-    if (ac < 65 || ac > 122) { 
-        console.log("wrong number");
-        return;
-    };
-
     // 코드가이드           65  66  67  68  69  70 ...
     const upperAsciiArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     // 코드가이드           97  98  99  100 101 102 ...
     const lowerAsciiArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    
-    for (let i = 0; i < asciiArray.length; i++) {
-        if (ac === asciiArray[i]) {
-            return asciiArray[i];
-        }
-    }
+
+    if (ac >= 65 && ac <= 90) return upperAsciiArr[ac - 65]
+    if (ac >= 97 && ac <= 122) return lowerAsciiArr[ac - 97]
 }
