72dc83f94851f4f26aabb72c6bdc6c00ddaf10ce
coldMater <coldmater@gmail.com>
2021-12-11 02:03:09 +0900

    변수 선언자 변경

--- playground/get_char_from_ascii.js
+++ playground/get_char_from_ascii.js
@@ -22,8 +22,8 @@ function getFromAscii(ac){
         return;
     };
 
-    let upperAsciiArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    let lowerAsciiArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const upperAsciiArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    const lowerAsciiArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     
     for (let i = 0; i < asciiArray.length; i++) {
         if (ac === asciiArray[i]) {
