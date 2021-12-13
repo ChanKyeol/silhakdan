cf3ed72f04029b527708d94cea057d531f2cc448
coldflow <dev.coldflow@gmail.com>
2021-12-14 01:08:07 +0900

    코딩 스타일 적용, if for 등의 소괄호 옆 공백

--- playground/get_char_from_ascii.js
+++ playground/get_char_from_ascii.js
@@ -2,21 +2,21 @@
 
 function printNum(num) {
     let willPrint = "";
-    for(let i = 0; i <= num - 1; i++){
+    for (let i = 0; i <= num - 1; i++) {
         willPrint += num;
     }
-return willPrint;
+    return willPrint;
 }
 
 
 
 function muchNum(num) {
-    for(let i = 1; i <= num; i++){
+    for (let i = 1; i <= num; i++) {
         console.log(printNum(i));
     }
 }
 
-function getFromAscii(ac){
+function getFromAscii(ac) {
     let asciiArray = ['A','B','C','D','E','F','G','H','I',
                  'J','K','L','M','N','O','P','Q','R',
                  'S','T','U','V','W','X', 'Y', 'Z',
@@ -25,7 +25,7 @@ function getFromAscii(ac){
                  'o', 'p', 'q', 'r', 's', 't', 'u', 
                  'v', 'w', 'x', 'y', 'z'];
     
-    if(ac < 65){
+    if (ac < 65) {
         console.log("wrong number");
         return;
     } 
@@ -34,8 +34,8 @@ function getFromAscii(ac){
         return;
     } 
         
-    for(let i = 0; i <= asciiArray.length - 1; i ++){
-            if(ac === asciiArray[i]){
+    for (let i = 0; i <= asciiArray.length - 1; i ++) {
+            if (ac === asciiArray[i]) {
                 return asciiArray[i];
             }
         }
