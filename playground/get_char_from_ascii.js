549b3ca157b911cb226c9c385f20acce8fa8ec44
coldflow <dev.coldflow@gmail.com>
2021-12-14 01:17:58 +0900

    ASCII 배열 재정의

--- playground/get_char_from_ascii.js
+++ playground/get_char_from_ascii.js
@@ -17,14 +17,10 @@ function muchNum(num) {
 }
 
 function getFromAscii(ac) {
-    let asciiArray = ['A','B','C','D','E','F','G','H','I',
-                 'J','K','L','M','N','O','P','Q','R',
-                 'S','T','U','V','W','X', 'Y', 'Z',
-                 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-                 'o', 'p', 'q', 'r', 's', 't', 'u', 
-                 'v', 'w', 'x', 'y', 'z'];
-    
+
+    let upperAsciiArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z'];
+    let lowerAsciiArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
     if (ac < 65 || ac > 122) {
         console.log("wrong number");
         return;
