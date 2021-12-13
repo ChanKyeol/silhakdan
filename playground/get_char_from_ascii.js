779d245cd671ccc13c028461beec5e60c2444011
coldflow <dev.coldflow@gmail.com>
2021-12-14 01:19:07 +0900

    변수 선언자 변경 let > const

--- playground/get_char_from_ascii.js
+++ playground/get_char_from_ascii.js
@@ -18,8 +18,8 @@ function muchNum(num) {
 
 function getFromAscii(ac) {
 
-    let upperAsciiArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z'];
-    let lowerAsciiArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const upperAsciiArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z'];
+    const lowerAsciiArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
     if (ac < 65 || ac > 122) {
         console.log("wrong number");
