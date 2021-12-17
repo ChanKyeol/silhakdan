9a9b20e4933886318aa2cc4f97f7b724bbfa4d18
coldflow <dev.coldflow@gmail.com>
2021-12-18 01:20:50 +0900

    fiborecurve 생성

--- playground/fibonacci_using_for.js
+++ playground/fibonacci_using_for.js
@@ -18,10 +18,4 @@ function fibo (idx) {
         
 }
 
-function fiboRecursive(idx) {
-  if(idx === 1) return 1;
-  if(idx === 2) return 1;
-  if(idx >= 3) return fiboRecursive(idx - 2) + fiboRecursive(idx - 1);
-}
 
-[3, 4, 5, 6, 20, 70].forEach((i) => console.log(i)); //forEach 사용으로 코드
---
+++ playground/fiborecursive.js
@@ -0,0 +1,6 @@
+function fiboRecursive(idx) {
+    if(idx === 1) return 1;
+    if(idx === 2) return 1;
+    if(idx >= 3) return fiboRecursive(idx - 2) + fiboRecursive(idx - 1);
+  }
+  
