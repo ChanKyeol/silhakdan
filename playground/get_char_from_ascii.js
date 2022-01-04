f17856f8048a1269a19a631eb5c8f94db099e3ad
coldflow <dev.coldflow@gmail.com>
2022-01-05 01:23:59 +0900

    chapter2_1 upload goodnightbro

--- playground/get_char_from_ascii.js
+++ playground/get_char_from_ascii.js
@@ -2,39 +2,34 @@
 
 function printNum(num) {
     let willPrint = "";
-    for(let i = 0; i <= num - 1; i++){
+    for (let i = 0; i <= num - 1; i++) {
         willPrint += num;
     }
-return willPrint;
+    return willPrint;
 }
 
 
 
 function muchNum(num) {
-    for(let i = 1; i <= num; i++){
+    for (let i = 1; i <= num; i++) {
         console.log(printNum(i));
     }
 }
 
-function getFromAscii(ac){
-    let asciiArray = ['A','B','C','D','E','F','G','H','I',
-                 'J','K','L','M','N','O','P','Q','R',
-                 'S','T','U','V','W','X', 'Y', 'Z',
-                 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-                 'o', 'p', 'q', 'r', 's', 't', 'u', 
-                 'v', 'w', 'x', 'y', 'z'];
-    
-    if(ac < 65){
+function getFromAscii(ac) {
+
+    //코드가이드            65  66  67  68  69  70  71  71  ...                                                                 90
+    const upperAsciiArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    //코드가이드            97  98  99  100 101 102 103 104 ...                                                                 122
+    const lowerAsciiArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+    if (ac < 65 || ac > 122) {
         console.log("wrong number");
-    } else if (ac > 122) {
-        console.log("wrong number");
-    } else {
-        for(let i = 0; i <= asciiArray.length - 1; i ++){
-            if(ac === asciiArray[i]){
-                return asciiArray[i];
-            }
-        }
-    }
+        return;
+    } 
+
+    if (ac >= 65 && ac <= 90) return upperAsciiArr[ac - 65];
+    if (ac >= 97 && ac <= 122) return lowerAsciiArr[ac - 97];
+   
     
 }
